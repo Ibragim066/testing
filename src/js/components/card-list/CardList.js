4c41b0ef0429e5f20be09505d72a5e004a7de997
Ibragim <gamemega.ao@gmail.com>
2024-02-16 11:57:18 +0300

    add components

---
+++ src/js/components/card-list/CardList.js
@@ -0,0 +1,46 @@
+import visaElectron from '../../../img/visa_electron_curved.png';
+import MasterCard from '../../../img/mastercard_curved.png';
+import AmexCard from '../../../img/credit_card_amex.png';
+import DiscoverCard from '../../../img/discover_straight_32px.png';
+import MirCard from '../../../img/mir_card.png';
+import DinersClubCard from '../../../img/diners_club_card.png';
+
+import './card-list.css';
+
+export default class CardsList {
+    constructor(container) {
+        if (!(container instanceof HTMLElement)) {
+            throw new Error('container is not HTMLElement');
+        }
+
+        this.container = container;
+        this.cards = [
+            { class: 'visa', title: 'Visa', url: visaElectron },
+            { class: 'master', title: 'Mastercard', url: MasterCard },
+            { class: 'amex', title: 'American Express', url: AmexCard },
+            { class: 'discover', title: 'Discover', url: DiscoverCard },
+            { class: 'mir', title: 'Mir', url: MirCard },
+            { class: 'diners_club', title: 'Diners Club', url: DinersClubCard },
+        ];
+    }
+
+    bindToDOM() {
+        this.container.innerHTML = this.markup;
+    }
+
+    get markup() {
+        return `
+      <div class="cards">
+        ${this.cards
+                .map(
+                    (card) => `
+        <div>
+          <img class="card ${card.class}" src=${card.url} alt="${card.title}">
+        </div>  
+        `,
+                )
+                .join('')}
+      </div>
+    `;
+    }
+}
---
+++ src/js/components/check-form/CheckForm.js
@@ -0,0 +1,98 @@
+import findPaymentSystem from '../../validation/findPaymentSystem';
+import isValid from '../../validation/isValid';
+import './check-form.css';
+
+export default class CheckForm {
+    constructor(container) {
+        if (!(container instanceof HTMLElement)) {
+            throw new Error('container is not HTMLElement');
+        }
+
+        this.container = container;
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    bindToDOM() {
+        this.container.innerHTML = CheckForm.markup;
+
+        const {
+            selector, messageSelector, submitSelector, inputSelector,
+        } = CheckForm;
+
+        this.element = this.container.querySelector(selector);
+        this.message = this.element.querySelector(messageSelector);
+        this.submit = this.element.querySelector(submitSelector);
+        this.input = this.element.querySelector(inputSelector);
+
+        this.element.addEventListener('submit', this.onSubmit);
+        this.input.addEventListener('input', this.onInputChange);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const isValidCard = isValid(this.input.value);
+        this.input.classList.toggle('valid', isValidCard);
+        this.input.classList.toggle('invalid', !isValidCard);
+
+        if (isValidCard) {
+            const paymentSystem = findPaymentSystem(this.input.value);
+            const card = document.querySelector(`.${paymentSystem}`);
+
+            if (card) {
+                card.classList.add('card-active');
+            } else {
+                this.showMessage();
+            }
+        }
+    }
+
+    onInputChange() {
+        this.input.classList.remove('invalid', 'valid');
+        const activeCard = document.querySelector('.card-active');
+        if (activeCard) {
+            activeCard.classList.remove('card-active');
+        }
+        this.hideMessage();
+    }
+
+    showMessage() {
+        this.message.classList.remove('hidden');
+    }
+
+    hideMessage() {
+        this.message.classList.add('hidden');
+    }
+
+    static get markup() {
+        return `
+      <form id="form" class="form">
+          <div class="form-group">
+              <input class="form-control" id="card_number" name="card_number" type="text" placeholder="Credit card number" data-original-title="" title="">
+          </div>
+          <div class='button-wrapper'>
+            <button class="submit">Click to Validate</button>
+          </div> 
+          <span class="message hidden">Упсс! Платежной системы нет в базе</span>
+      </form>
+    `;
+    }
+
+    static get selector() {
+        return '.form';
+    }
+
+    static get messageSelector() {
+        return '.message';
+    }
+
+    static get submitSelector() {
+        return '.submit';
+    }
+
+    static get inputSelector() {
+        return '.form-control';
+    }
+}
